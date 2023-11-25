1c87a9eb47dcb20227f1944d17b1c50b98512269
ibrahim bayburtlu <ibrahimbayburtlu5gmail.com>
2023-11-25 20:36:44 +0300

    Online Shopping Application

---
+++ order-service/src/main/java/com/ibrahimbayburtlu/orderservice/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.ibrahimbayburtlu.orderservice.dto;
+
+import com.ibrahimbayburtlu.orderservice.dto.OrderLineItemsDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
